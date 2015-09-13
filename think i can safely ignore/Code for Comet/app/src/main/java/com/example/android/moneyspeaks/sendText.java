817a6eeca43a3d1bf9b7eaf1dd7e42d152b7fa5d
Sunny Rekhi <rohit.rekhi@gmail.com>
2015-09-13 03:06:40 -0500

    deleted unnecessary folder

--- think i can safely ignore/Code for Comet/app/src/main/java/com/example/android/moneyspeaks/sendText.java
+++
@@ -1,7 +0,0 @@
-package com.example.android.moneyspeaks;
-
-/**
- * Created by Sunny on 6/11/2015.
- */
-public class sendText {
-}
--- think i can safely ignore/MoneySpeaks/app/src/main/java/com/example/android/moneyspeaks/GetUserCallback.java
+++
@@ -1,8 +0,0 @@
-package com.example.android.moneyspeaks;
-
-/**
- * Created by Sunny on 7/3/2015.
- */
-interface GetUserCallback {
-    public abstract void done (User returnedUser);
-}
